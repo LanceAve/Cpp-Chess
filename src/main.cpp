3e700426e83fabd7aaf91c819c023be38652151f
LanceAve <lanceave@protonmail.com>
2025-05-28 14:12:17 -0400

    testing movePiece
    Result:
    Initial Board:
    . . . . . . . .
    p p p p p p p p
    . . . . . . . .
    . . . . . . . .
    . . . . . . . .
    . . . . . . . .
    P P P P P P P P
    . . . . . . . .
    
    Board After Move:
    . . . . . . . .
    p p p p p p p p
    . . . . . . . .
    . . . . . . . .
    P . . . . . . .
    . . . . . . . .
    . P P P P P P P
    . . . . . . . .

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,16 @@ using namespace std;
 
 int main() {
     Board board;
+
+    std::cout << "Initial Board:\n";
     board.printBoard();
+
+    // testing pawn movement from (6,0) to (4,0)
+    std::cout << "\nMoving white pawn from (6,0) to (4,0)...\n";
+    board.movePiece(6, 0, 4, 0);  // white pawn up two spaces
+
+    std::cout << "\nBoard After Move:\n";
+    board.printBoard();
+
     return 0;
 }
