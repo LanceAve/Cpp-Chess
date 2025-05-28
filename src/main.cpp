28a5644fbad97ef3b16c201c36ac2ed8777affb7
LanceAve <lanceave@protonmail.com>
2025-05-28 14:45:41 -0400

    set local to UTF-8 on Windows platforms

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include "Board.hpp"
+#include <windows.h>  // Only on Windows
+
 using namespace std;
-#include "Board.hpp";
 
 // This will follow the rank-file system (rows first, columns second)
-
 int main() {
+// set locale to UTF-8 to represent ASCII chess pieces
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);  // Enable UTF-8 output on Windows console
+#endif
+
+    std::locale::global(std::locale(""));
+    std::wcout.imbue(std::locale());
+
     Board board;
 
     std::cout << "Initial Board:\n";
