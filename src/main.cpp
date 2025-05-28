be52bc2337074711687bccda50e27f4fc78b07e9
LanceAve <lanceave@protonmail.com>
2025-05-28 13:54:20 -0400

    added printBoard to main to test method

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include "Board.hpp";
+
+// This will follow the rank-file system (rows first, columns second)
+
+int main() {
+    Board board;
+    board.printBoard();
+    return 0;
+}
