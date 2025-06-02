0af7fe48f833aed099d3be8e808cdb5ac9e1275d
LanceAve <lanceave@protonmail.com>
2025-06-02 13:22:32 -0400

    testing movement of white pawn from e2 to e4

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 // This will follow the rank-file system (rows first, columns second)
 int main() {
-// set locale to UTF-8 to represent ASCII chess pieces
+
+    // set locale to UTF-8 to represent ASCII chess pieces
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);  // Enable UTF-8 output on Windows console
 #endif
@@ -15,6 +16,17 @@ int main() {
     std::wcout.imbue(std::locale());
 
     Board board;
+    board.setupBoard();     // initialize starting positions
+    std::wcout << L"Initial Chess Board:\n";
+    board.printBoard();     // show current board state
+
+    // Example move: white pawn from e2 to e4
+    if (board.isValidMove(6, 4, 4, 4)) {
+        board.movePiece(6, 4, 4, 4);
+        std::wcout << L"\nBoard After Move (e2 to e4):\n";
+        board.printBoard();
+    } else {
+        std::wcout << L"Invalid move.\n";
     }
 
     return 0;
