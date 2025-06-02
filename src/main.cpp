022d89888a1fe3085874e0c13a58a50034b1d48e
LanceAve <lanceave@protonmail.com>
2025-06-02 13:17:10 -0400

    removed testing board setup from main

--- src/main.cpp
+++ src/main.cpp
@@ -15,35 +15,7 @@ int main() {
     std::wcout.imbue(std::locale());
 
     Board board;
-
-    // Set up white pieces
-    board.getSquareAt(7, 0)->setPiece(new Piece{rook, Color::white});
-    board.getSquareAt(7, 1)->setPiece(new Piece{knight, Color::white});
-    board.getSquareAt(7, 2)->setPiece(new Piece{bishop, Color::white});
-    board.getSquareAt(7, 3)->setPiece(new Piece{queen, Color::white});
-    board.getSquareAt(7, 4)->setPiece(new Piece{king, Color::white});
-    board.getSquareAt(7, 5)->setPiece(new Piece{bishop, Color::white});
-    board.getSquareAt(7, 6)->setPiece(new Piece{knight, Color::white});
-    board.getSquareAt(7, 7)->setPiece(new Piece{rook, Color::white});
-    for (int col = 0; col < 8; ++col) {
-        board.getSquareAt(6, col)->setPiece(new Piece{pawn, Color::white});
     }
-
-    // Set up black pieces
-    board.getSquareAt(0, 0)->setPiece(new Piece{rook, Color::black});
-    board.getSquareAt(0, 1)->setPiece(new Piece{knight, Color::black});
-    board.getSquareAt(0, 2)->setPiece(new Piece{bishop, Color::black});
-    board.getSquareAt(0, 3)->setPiece(new Piece{queen, Color::black});
-    board.getSquareAt(0, 4)->setPiece(new Piece{king, Color::black});
-    board.getSquareAt(0, 5)->setPiece(new Piece{bishop, Color::black});
-    board.getSquareAt(0, 6)->setPiece(new Piece{knight, Color::black});
-    board.getSquareAt(0, 7)->setPiece(new Piece{rook, Color::black});
-    for (int col = 0; col < 8; ++col) {
-        board.getSquareAt(1, col)->setPiece(new Piece{pawn, Color::black});
-    }
-
-    std::cout << "Initial Board:" << std::endl;
-    board.printBoard();
 
     return 0;
 }
